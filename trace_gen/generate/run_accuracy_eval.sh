89b5781bffa81ffa508123c5c6d646ffec754278
Donghyun Kim <donghyun@utexas.edu>
2024-12-16 20:10:48 -0600

    heatmap generation bash script

--- trace_gen/generate/run_accuracy_eval.sh
+++ trace_gen/generate/run_accuracy_eval.sh
@@ -1,23 +1,21 @@
-PRETRAINED_MODEL_PATH="<BASE MODEL PATH w>"
-MERGED_MODEL_PATH="<FILLME>"
-ADAPTER_PATH="<FILLME>"
-CKPT="checkpoint"
-GPU=1
-PORT=8001
+PRETRAINED_MODEL_PATH="TraceLLM/checkpoints/llama-2-7b"
+MERGED_MODEL_PATH="${PRETRAINED_MODEL_PATH}/merged"
+CKPT_PATH="baseline"
+GEN_TYPE="recursive_instruction"  # {baseline, recursive, recursive_instruction}
+GPU=0
+PORT=8000
 
-CKPT_PATH="checkpoint-name"
-MERGED_MODEL_NAME=${CKPT}_${CKPT_PATH}
-if [ ! -d "${MERGED_MODEL_PATH}/${MERGED_MODEL_NAME}" ]; then
-  CUDA_VISIBLE_DEVICES=$GPU python generate/load_and_merge.py --model_name_or_path=${PRETRAINED_MODEL_PATH} --lora_path=${ADAPTER_PATH}/${CKPT}/${CKPT_PATH} --output_path=${MERGED_MODEL_PATH}/${MERGED_MODEL_NAME}
-  echo "merged adapter"
-  cp ${PRETRAINED_MODEL_PATH}/tokenizer* ${MERGED_MODEL_PATH}/${MERGED_MODEL_NAME}
-  cp ${PRETRAINED_MODEL_PATH}/special_tokens_map.json ${MERGED_MODEL_PATH}/${MERGED_MODEL_NAME}
-  echo "copied tokenizer"
+if [ ! -d "${MERGED_MODEL_PATH}/${CKPT_PATH}" ]; then
+  # Merge lora adapters
+  CUDA_VISIBLE_DEVICES=$GPU python generate/load_and_merge.py --model_name_or_path=${PRETRAINED_MODEL_PATH} --lora_path=${PRETRAINED_MODEL_PATH}/${CKPT_PATH} --output_path=${MERGED_MODEL_PATH}/${CKPT_PATH}
+  cp ${PRETRAINED_MODEL_PATH}/tokenizer* ${MERGED_MODEL_PATH}/${CKPT_PATH}
+  cp ${PRETRAINED_MODEL_PATH}/special_tokens_map.json ${MERGED_MODEL_PATH}/${CKPT_PATH}
 fi
-CUDA_VISIBLE_DEVICES=$GPU python -m vllm.entrypoints.api_server --port $PORT --model ${MERGED_MODEL_PATH}/${MERGED_MODEL_NAME} --gpu-memory-utilization 0.97 &
-sleep 15
+
+CUDA_VISIBLE_DEVICES=$GPU python -m vllm.entrypoints.api_server --port $PORT --model ${MERGED_MODEL_PATH}/${CKPT_PATH} --gpu-memory-utilization 0.95 &
+sleep 30
 for temp in 0.0 0.2 0.4 0.6 0.8 1.0
 do
-  python generate/trace_oracle/run_validation_heatmap.py --port $PORT --summary-path heatmap_${MERGED_MODEL_NAME}_temp_${temp}.summary --temperature ${temp} --tag heatmap_${MERGED_MODEL_NAME} --gen-type recursive_instruction
+  python generate/trace_oracle/run_validation_heatmap.py --port $PORT --summary-path heatmap_${CKPT_PATH}_temp_${temp}.summary --temperature ${temp} --tag heatmap_${CKPT_PATH} --gen-type $GEN_TYPE
 done
 kill %%
